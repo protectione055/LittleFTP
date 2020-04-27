f1cfc98225f0f1f3672bd2477263e8fbfe7ee509
protectione055 <protectione055@foxmail.com>
2020-04-27 17:09:14 +0800

    first commit

---
+++ ftpclient/ftpclient.c
@@ -0,0 +1,71 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<WinSock2.h>
+
+#define FILENAME_SIZE 50
+#define BUFF_SIZE 1024
+
+void error(const char*);
+
+int main(void) {
+
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	/*Create clientSock*/
+	SOCKET clientSock = socket(PF_INET, SOCK_STREAM, 0);
+
+	/*Input the ip*/
+	char ip[20];
+	fputs("Please input server's ip: ", stdout);
+	fgets(ip, sizeof(ip), stdin);
+
+	/*Config the servAddr*/
+	SOCKADDR_IN servAddr;
+	memset(&servAddr, 0, sizeof(SOCKADDR_IN));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(2200);
+	servAddr.sin_addr.s_addr = inet_addr(ip);
+
+	if (connect(clientSock, (SOCKADDR*)&servAddr, sizeof(SOCKADDR_IN)) < 0) error("Connect error");
+	printf("Successfully connected to the server %s\n", ip);
+
+	char filename[FILENAME_MAX];
+	short flag = 0;
+	/*Determine whatever the file was opened successfully on server*/
+	while (!flag) {
+		printf("Please input the filename: ");
+		scanf("%s", filename);
+
+		send(clientSock, filename, sizeof(filename), 0);
+
+		if (recv(clientSock, (char*)&flag, sizeof(short), 0) < 0) error("Receive error");//receive the flag message from server
+		else printf("flag = %hd\n", flag);
+	}
+
+	FILE* file = fopen(filename, "wb");
+	if (file == NULL) error("Failed to open file");
+	else printf("Start to receive the file...\n");
+
+	int rc;
+	char buff[BUFF_SIZE] = { 0 };
+	do {
+		rc = recv(clientSock, buff, sizeof(buff), 0);
+		printf("%dbytes received\n", rc);
+		if(fwrite(buff, 1, rc, file) < 0) error("File write error");
+	} while (rc > 0);
+	printf("File received successfully\n");
+	
+	fclose(file);
+	closesocket(clientSock);
+	WSACleanup();
+	return 0;
+	
+}
+
+void error(const char* message) {
+	fprintf(stderr, "%s\n", message);
+	system("pause");
+	exit(1);
+}
---
+++ ftpserver/ftpserver.c
@@ -0,0 +1,76 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include<winsock2.h>
+
+#define MAX_SIZE 1024
+
+void error(const char*);
+
+int main(void) {
+
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET servSock = socket(PF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN servAddr;
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = PF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(2200);
+
+	if (bind(servSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == -1) error("Bind error");
+
+	if (listen(servSock, 1) == -1) error("Listen error");
+	printf("FTP server is running on port: 2200\n");
+	printf("Waiting for client...\n");
+
+	SOCKADDR_IN clientAddr;
+	int szSockaddr = sizeof(SOCKADDR_IN);
+	SOCKET clientSock = accept(servSock, (SOCKADDR*)&clientAddr, &szSockaddr);
+	if (clientSock == INVALID_SOCKET) error("clientSocket accpet error");
+	printf("Client %s connected.\n", inet_ntoa(clientAddr.sin_addr));
+
+	short flag = 0;
+	FILE* file = NULL;
+	char buff[MAX_SIZE];
+	while (!flag) {
+		recv(clientSock, buff, sizeof(buff), 0);
+		char* filename = strdup(buff);//Retrive the file name
+		file = fopen(filename, "rb");
+		if (file != NULL) {
+			flag = 1;
+			send(clientSock, (char*)&flag, sizeof(short), 0);
+		}
+		else {
+			printf("Failed to open file %s\n%s\n", filename, strerror(errno));
+			memset(buff, 0, sizeof(buff));
+		}
+	}
+		 
+
+	printf("Starting to transmit the file...\n");
+
+	int rc = 0;
+	while ((rc = fread(buff, 1, sizeof(buff), file)) > 0) {
+		printf("%dbytes sended\n", rc);
+		if (send(clientSock, buff, rc, 0) < 0) error("Send error");
+	}
+
+	printf("Transmission completed.\n");
+
+	fclose(file);
+	closesocket(servSock);
+	closesocket(clientSock);
+	WSACleanup();
+	return 0;
+
+}
+
+void error(const char* message) {
+	fprintf(stderr, message);
+	system("pause");
+	exit(1);
+}
